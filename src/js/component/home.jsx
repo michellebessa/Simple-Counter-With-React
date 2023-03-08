68fd4a79b9d01dec10e730baf7a09c00c50df959
michellebessa <michelleandressa12@gmail.com>
2023-03-08 23:18:19 +0000

    home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Box } from "./box"
-// import { Counter } from "./SecondsCounter"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
 		<React.Fragment>
 		<main className="container">
 		<Box/>
-		{/* <Counter/> */}
 		</main>
 		</React.Fragment>
 	);
